9bc70e6e4e8d6f5618153aa1fd36ca5cbc54f5fe
palmnuttawut <palmnuttawut1997@gmail.com>
2018-06-26 11:40:03 +0700

    Hello World

---
+++ src/Lab1/myCode.java
@@ -0,0 +1,20 @@
+package Lab1;
+
+public class myCode {
+
+
+    public static void main(String[] args) {
+
+
+
+        System.out.println("Hello World");
+        System.out.println("name:Nuttawut Wesnusit");
+        System.out.println("Id:032");
+        System.out.println("group: ระบบสาระสนเทศ");
+        System.out.println("university: มทร.");
+
+
+
+
+    }//main
+}//class
